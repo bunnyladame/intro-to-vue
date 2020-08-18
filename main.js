0ba940e735c583f857354cae6e7121b8b9869697
Eva Deane <bunny@kfs.org>
2020-08-18 15:14:10 -0600

    v-bind and v-if/else

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'A pair of warm, fuzzy socks.'
+        description: 'A pair of warm, fuzzy socks.',
+        image: './assets/socks.jpg',
+        link: 'https://www.google.com',
+        inStock: true
     }
 })
